38fee399f9f76002ac1d983d622d9645fad30940
Arnault Chazareix <arnaultc@sicara.com>
2022-01-11 02:00:36 +0100

    add support for coco train, cache results and improve loading and deal with double filtering

--- explore_annotations.py
+++ explore_annotations.py
@@ -1,11 +1,13 @@
-from typing import List
+from typing import List, Literal, Optional
 
 import pandas as pd
 import streamlit as st
 from stqdm import stqdm
 
 from src.constants.colors import COLOR_MAP
-from src.utils.display import load_and_annotate_image, load_crop
+from src.utils.display import HOUR, load_and_annotate_image, load_crop
+
+Subset = Literal["train", "val"]
 
 st.set_page_config(
     "Explore coco dataset val set",
@@ -13,9 +15,50 @@ st.set_page_config(
 )
 
 
-@st.experimental_memo
-def load_all_annotations():
-    return pd.read_csv("data/annotations/coco_val_2020.csv")
+@st.experimental_memo(ttl=1 * HOUR)
+def load_all_annotations(subset: Subset) -> pd.DataFrame:
+    return pd.read_parquet(f"data/annotations/coco_{subset}_2020.parquet.gzip")
+
+
+@st.experimental_memo()
+def get_category_count(subset: Subset) -> pd.DataFrame:
+    return load_all_annotations(subset).category_name.value_counts()
+
+
+@st.experimental_memo(ttl=6 * HOUR)
+def cached_isin(series: pd.Series, elements: List[str]) -> pd.Series:
+    return series.isin(elements)
+
+
+@st.experimental_memo(ttl=3 * HOUR)
+def get_selected_annotations(
+    subset: Subset, selected_images: Optional[List[str]], selected_categories: List[str]
+) -> pd.DataFrame:
+    all_annotations = load_all_annotations(subset)
+
+    if selected_images is None:
+        selected_annotations_for_image = True
+    else:
+        selected_annotations_for_image = cached_isin(
+            all_annotations.image_name, selected_images
+        )
+    selected_annotations_for_category = cached_isin(
+        all_annotations.category_name, selected_categories
+    )
+    return all_annotations.loc[
+        selected_annotations_for_image & selected_annotations_for_category
+    ]
+
+
+@st.experimental_memo(ttl=3 * HOUR)
+def get_selected_images(
+    subset: Subset, selected_images: Optional[List[str]], selected_categories: List[str]
+) -> pd.DataFrame:
+    all_annotations = load_all_annotations(subset)
+    selected_images = get_selected_annotations(
+        subset, selected_images, selected_categories
+    ).image_name.drop_duplicates()
+    return all_annotations.loc[cached_isin(all_annotations.image_name, selected_images)]
 
 
 def get_arguments_from_query(
@@ -33,40 +76,32 @@ def get_arguments_from_query(
     return default_values
 
 
-annotations = load_all_annotations()
-
 with st.sidebar:
-    st.write(annotations.category_name.value_counts())
+    subset = st.selectbox("Subset", ["val", "train"])
+    category_count = get_category_count(subset)
+    st.write(get_category_count(subset))
     display_crops = st.checkbox("Display Crops")
-    if not display_crops and st.checkbox("Filter by image"):
-        images_to_display = st.multiselect(
-            "Images to display", annotations.image_name.unique()
+    available_labels = category_count.index.tolist()
+    images_to_display = st.multiselect(
+        "Images to display [Empty = All]",
+        available_labels,
+    )
+    if not images_to_display:
+        images_to_display = None
+    labels_to_display = st.multiselect(
+        "Labels to display",
+        available_labels,
+        get_arguments_from_query("label", available_labels, default_values=[]),
+    )
+    display_only_selected_labels = st.checkbox(
+        "Display only selected labels", value=False
+    )
+    if display_only_selected_labels:
+        annotations = get_selected_annotations(
+            subset, images_to_display, labels_to_display
         )
-        annotations = annotations.loc[lambda df: df.image_name.isin(images_to_display)]
     else:
-        available_labels = annotations.category_name.unique()
-        labels_to_display = st.multiselect(
-            "Labels to display",
-            available_labels,
-            get_arguments_from_query("label", available_labels, default_values=[]),
-        )
-        if display_crops:
-            annotations = annotations.loc[
-                lambda df: df.category_name.isin(labels_to_display)
-            ]
-        else:
-            display_only_selected_labels = st.checkbox(
-                "Display only selected labels", value=False
-            )
-            if display_only_selected_labels:
-                annotations = annotations.loc[
-                    lambda df: df.category_name.isin(labels_to_display)
-                ]
-            else:
-                annotations = annotations.groupby("image_name").filter(
-                    lambda g: g.category_name.isin(labels_to_display).any()
-                )
-
+        annotations = get_selected_images(subset, images_to_display, labels_to_display)
 
 if display_crops:
     for _, crop_annotations in stqdm(
@@ -77,8 +112,10 @@ if display_crops:
         coordinates = crop_annotations[["x1", "y1", "x2", "y2"]]
         st.image(load_crop(coco_url, coordinates, shape=(640, 480)), caption=image_name)
 else:
-    for (image_name, coco_url), image_annotations in annotations.groupby(
-        ["image_name", "coco_url"]
+    for (image_name, coco_url), image_annotations in stqdm(
+        annotations.groupby(["image_name", "coco_url"]),
+        desc="Displaying Images",
+        total=len(annotations[["image_name", "coco_url"]].drop_duplicates()),
     ):
         st.image(
             load_and_annotate_image(coco_url, image_annotations, color_map=COLOR_MAP),
